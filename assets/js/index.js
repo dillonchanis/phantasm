fd52fb5799ce542b6b3ce444a5fd860eca8ce1ee
Dillon Chanis <dillonchanis92@gmail.com>
2018-04-02 20:25:34 -0400

    Adding Back to Home button on blog page and removing AOS from JS file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,6 @@
-import AOS from 'aos'
 import hljs from 'highlight.js'
 import '../scss/main.scss'
 
 (function () {
-  AOS.init({
-    disable: 'mobile',
-    offset: 50,
-    duration: 1000,
-    easing: 'ease-out-quad'
-  })
-
   hljs.initHighlightingOnLoad()
 })()
